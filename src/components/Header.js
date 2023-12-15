324e75c0d64f86de8d7fd0999266a81ec472845c
G_T_Ayyy <147361574+gtayyy@users.noreply.github.com>
2023-12-14 16:58:36 -0800

    Complete styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from "react";
+import './Header.css';
 
 const Header = () => {
     return (
         <React.Fragment>
         <div className="header">
-            <h1>Forum Site</h1>
+            <h1>"   Forum Site   "</h1>
         </div>
         </React.Fragment>
     )
